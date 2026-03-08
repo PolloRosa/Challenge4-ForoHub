1f6e313ec7ab7b70c1fe9e5f3c38a2d5d91d22a4
PolloRosa <accarhuachagua@gmail.com>
2026-03-07 23:19:08 -0500

    Agrega Autenticación JWT
    Agrega dependencia java-jwt. Agrega controlador, servicio y DTOs para autenticación. Agrega clase SecurityConfigurations y clase filtro SecurityFilter para autenticar usuarios con validaciones para token y usuario eliminado o inactivo. Agrega la clase TokenService para la creación y lectura de token, con uso de atributos definidos en el application.properties.

--- src/main/java/com/pollorosa/forohub/domain/usuario/Usuario.java
+++ src/main/java/com/pollorosa/forohub/domain/usuario/Usuario.java
@@ -5,7 +5,11 @@ import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.List;
 
 @Table(name = "usuario")
@@ -14,7 +18,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
-public class Usuario /*implements UserDetails*/ {
+public class Usuario implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -25,8 +29,40 @@ public class Usuario /*implements UserDetails*/ {
 
     private String contrasena;
 
-    /*@OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-    private List<Perfil> perfiles;*/
-
     private Boolean activo;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return this.contrasena;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.correoElectronico;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
